2834af47e2b227ce9d3e8c20ef2c10964c7b26eb
TolyaKravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2020-05-12 12:41:08 +0300

    send string on send message, add onopen method

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -1,17 +1,24 @@
+import {addResponseMessage} from "react-chat-widget";
+
 class SocketService {
   constructor(chatId) {
     this.chatId = chatId;
     this.socket = new WebSocket(
       "ws://172.16.6.253:19001?token=383ce66f9cae40bfb7b759b63a636e15"
     );
+
+    this.socket.onopen = () =>{
+      addResponseMessage('соединение установлено');
+    }
   }
 
   sendMessage(message) {
+      let objectMessage = {
+          "chatId": this.chatId,
+          "message": message,
+      }
     //TODO attach chatId to message payload if needed. Discuss with Anton
-    this.socket.send({
-        "chatId": this.chatId,
-        "message": message,
-    });
+    this.socket.send(JSON.stringify(objectMessage));
   }
 
   listenToMessage(callback) {
